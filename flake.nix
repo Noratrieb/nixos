f33a89c6ed1ebcf8cf96625c7f65e0808270bad1
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2023-05-26 21:27:02 +0200

    more

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    # nur.url = github:nix-community/NUR;
+    nur.url = github:nix-community/NUR;
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-22.11";
@@ -34,7 +34,6 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#nils@nixos'
     homeConfigurations = {
-      # FIXME replace with your username@hostname
       "nils@nixos" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
