b7f955b93bc83a2619ad5a28261145235846c4ee
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2024-03-02 20:26:21 +0100

    typos

--- custom-pkgs/crates/list.nix
+++ custom-pkgs/crates/list.nix
@@ -11,4 +11,10 @@ pkgs: [
     downloadHash = "sha256-zLQHUHo3e/nY5UHhX03nK/gr+0rap/bSSlX7HDzr9uo=";
     cargoHash = "sha256-DVuIPO/dCk5rvPBzfM8lGGvM1ZZUUBgN7jSr/ZRGUwE=";
   }
+  {
+    pname = "typos-cli";
+    version = "1.19.0";
+    downloadHash = "sha256-qB0ISYBvblOGmoj1TUA9tddMHk9iDk0R3vlOWclQcMg=";
+    cargoHash = "sha256-s9L+VL37ivoEo5j6Z/JRApH/p4p5E+vl0i1HWWf4g5o=";
+  }
 ]
