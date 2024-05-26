fdd407258001a453ed23661c8d12b6d7300bd112
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2024-05-26 17:17:30 +0200

    add `run` binary

--- custom-pkgs/default.nix
+++ custom-pkgs/default.nix
@@ -1,6 +1,7 @@
 pkgs: {
   cargo-bisect-rustc = import ./cargo-bisect-rustc/default.nix pkgs;
   monaspace = import ./monaspace.nix pkgs;
+  run = import ./run { inherit pkgs; };
   x = import ./x { inherit pkgs; };
 }
 
