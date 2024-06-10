749337fc681ec9f153f411fa4bdc695026176c9a
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2024-06-10 19:36:44 +0200

    Add pipewire plugin to OBS

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -27,13 +27,17 @@ in
     homeDirectory = "/home/nora";
   };
 
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [ obs-pipewire-audio-capture ];
+  };
+
   home.packages = with pkgs; [
     audacity
     customPkgs.cargo-bisect-rustc
     customPkgs.run
     discord
     jetbrains.idea-ultimate
-    obs-studio
     obsidian
     prismlauncher
     spotify
