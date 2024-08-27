5852644ee3f4ff6dedaaf18c1687831c78b6ccaf
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2024-08-27 22:49:26 +0200

    ecdsa-sha2-nistp256 host key

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -218,6 +218,22 @@ in
 
   services.openssh = {
     enable = true;
+    hostKeys = [
+      {
+        bits = 4096;
+        path = "/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+      }
+      {
+        # P256
+        path = "/etc/ssh/ssh_host_ecdsa_key";
+        type = "ecdsa";
+      }
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
     settings.PermitRootLogin = "no";
     settings.PasswordAuthentication = false;
   };
