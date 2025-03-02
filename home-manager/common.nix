ceabd490e64b2d0426b9bb4d4495ed229c884426
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-03-02 21:40:45 +0100

    hyfetch

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -69,6 +69,26 @@ in
     };
   };
 
+  programs.hyfetch = {
+    enable = true;
+    settings = {
+      preset = "transgender";
+      mode = "rgb";
+      light_dark = "dark";
+      lightness = 0.65;
+      color_align = {
+        mode = "horizontal";
+        custom_colors = [ ];
+        fore_back = null;
+      };
+      backend = "fastfetch";
+      args = null;
+      distro = null;
+      pride_month_shown = [ ];
+      pride_month_disable = false;
+    };
+  };
+
   programs.home-manager.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
