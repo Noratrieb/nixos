1235ffe614ab03a34618646224cacfdc2c8b9def
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-04-07 19:01:32 +0200

    bar??

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -1,73 +1,73 @@
-{ ... }: {
-  programs.waybar.enable = true;
-  programs.waybar.settings = {
-    mainBar = {
-      height = 35;
-      spacing = 4;
-      modules-left = [ ];
-      modules-center = [
-        "clock"
-      ];
-      modules-right = [
-        "privacy"
-        "pulseaudio"
-        "cpu"
-        "memory"
-        "tray"
-      ];
-      clock = {
-        format = "{:%a %F %H:%M}";
-        "tooltip-format" = "<tt><small>{calendar}</small></tt>";
-        "calendar" = {
-          "mode" = "year";
-          "mode-mon-col" = 3;
-          "weeks-pos" = "right";
-          "on-scroll" = 1;
-          "format" = {
-            "months" = "<span color='#ffead3'><b>{}</b></span>";
-            "days" = "<span color='#ecc6d9'><b>{}</b></span>";
-            "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
-            "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
-            "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+{ config, ... }: {
+  home.file."${config.xdg.configHome}/waybar/config.jsonc" = {
+    text =
+      builtins.toJSON {
+        height = 35;
+        spacing = 4;
+        modules-left = [ ];
+        modules-center = [
+          "clock"
+        ];
+        modules-right = [
+          "privacy"
+          "pulseaudio"
+          "cpu"
+          "memory"
+          "tray"
+        ];
+        clock = {
+          format = "{:%a %F %H:%M}";
+          "tooltip-format" = "<tt><small>{calendar}</small></tt>";
+          "calendar" = {
+            "mode" = "year";
+            "mode-mon-col" = 3;
+            "weeks-pos" = "right";
+            "on-scroll" = 1;
+            "format" = {
+              "months" = "<span color='#ffead3'><b>{}</b></span>";
+              "days" = "<span color='#ecc6d9'><b>{}</b></span>";
+              "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
+              "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
+              "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          "actions" = {
+            "on-click-right" = "mode";
+            "on-scroll-up" = "shift_up";
+            "on-scroll-down" = "shift_down";
           };
         };
-        "actions" = {
-          "on-click-right" = "mode";
-          "on-scroll-up" = "shift_up";
-          "on-scroll-down" = "shift_down";
+        pulseaudio = {
+          # "scroll-step": 1, // %, can be a float
+          "format" = "{volume}% {icon} {format_source}";
+          "format-bluetooth" = "{volume}% {icon} {format_source}";
+          "format-bluetooth-muted" = " {icon} {format_source}";
+          "format-muted" = " {format_source}";
+          "format-source" = "{volume}% ";
+          "format-source-muted" = "";
+          "format-icons" = {
+            "headphone" = "";
+            "hands-free" = "";
+            "headset" = "";
+            "phone" = "";
+            "portable" = "";
+            "car" = "";
+            "default" = [ "" "" "" ];
+          };
+          "on-click" = "pavucontrol";
         };
-      };
-      pulseaudio = {
-        # "scroll-step": 1, // %, can be a float
-        "format" = "{volume}% {icon} {format_source}";
-        "format-bluetooth" = "{volume}% {icon} {format_source}";
-        "format-bluetooth-muted" = " {icon} {format_source}";
-        "format-muted" = " {format_source}";
-        "format-source" = "{volume}% ";
-        "format-source-muted" = "";
-        "format-icons" = {
-          "headphone" = "";
-          "hands-free" = "";
-          "headset" = "";
-          "phone" = "";
-          "portable" = "";
-          "car" = "";
-          "default" = [ "" "" "" ];
+
+        cpu = {
+          format = "{usage}% ";
+          tooltip = false;
         };
-        "on-click" = "pavucontrol";
-      };
+        memory = {
+          format = "{}% ";
+        };
+        tray = {
+          spacing = 10;
+        };
 
-      cpu = {
-        format = "{usage}% ";
-        tooltip = false;
       };
-      memory = {
-        format = "{}% ";
-      };
-      tray = {
-        spacing = 10;
-      };
-
-    };
   };
 }
