5dcc4f4f238b8e4e2008e94f34e35d1f14a06724
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-04-19 13:01:47 +0200

    update

--- custom-pkgs/crates/list.nix
+++ custom-pkgs/crates/list.nix
@@ -3,18 +3,18 @@ pkgs: [
     pname = "cargo-dl";
     version = "0.1.4";
     downloadHash = "sha256-txSRmOr3tYw72xI5Dqt/y1EboaqFWEMN+lASVXLNdgQ=";
-    cargoHash = "sha256-mkRZtC6JjsJxQ4DTjS2qw2y1opFIMt39eEMR1DurgAM=";
+    cargoHash = "sha256-bS1al2UnfUxCRjQYfvUKx19WW6sjGIwno78bORd3uBQ=";
   }
   {
     pname = "counts";
     version = "1.0.4";
     downloadHash = "sha256-zLQHUHo3e/nY5UHhX03nK/gr+0rap/bSSlX7HDzr9uo=";
-    cargoHash = "sha256-DVuIPO/dCk5rvPBzfM8lGGvM1ZZUUBgN7jSr/ZRGUwE=";
+    cargoHash = "sha256-pGrXn5/FD9RUbzt5DWM6LkEo0oHmosWy023Vd+EhcGY=";
   }
   {
     pname = "typos-cli";
     version = "1.19.0";
     downloadHash = "sha256-qB0ISYBvblOGmoj1TUA9tddMHk9iDk0R3vlOWclQcMg=";
-    cargoHash = "sha256-s9L+VL37ivoEo5j6Z/JRApH/p4p5E+vl0i1HWWf4g5o=";
+    cargoHash = "sha256-ja4KrF2JdB2SVqnImdY7qdYXOJJgSftWtDCqWQ3ImYs=";
   }
 ]
