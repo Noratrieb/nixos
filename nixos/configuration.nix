9c469bbaef926be871b52ff6971d0ba6fa5ecc2f
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-04-21 17:53:03 +0200

    paperless backup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -233,7 +233,7 @@ in
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0n1ikUG9rYqobh7WpAyXrqZqxQoQ2zNJrFPj12gTpP"
         ];
-        extraGroups = [ "wheel" "networkmanager" "audio" "libvirtd" ];
+        extraGroups = [ "wheel" "networkmanager" "audio" "libvirtd" "docker" ];
         packages = with pkgs; [
           vscode
           chromium
@@ -245,6 +245,28 @@ in
       vboxusers = {
         members = [ "nora" ];
       };
+    };
+  };
+
+  systemd.services.paperless-ngx-backup = {
+    description = "paperless-ngx data backup to NAS";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "nora";
+      ExecStart = ''
+        ${lib.getExe pkgs.rsync} -a -v --delete --exclude=redis /home/nora/.local/share/paperless-ngx/ /mnt/nas/HEY/_Nora/paperless/backup
+      '';
+    };
+  };
+  systemd.timers.paperless-ngx-backup = {
+    description = "paperless-ngx data backup to NAS";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      Unit = "paperless-ngx-backup.service";
+      OnCalendar = "daily UTC";
+      RandomizedDelaySec = 1800;
+      FixedRandomDelay = true;
+      Persistent = true; # ensure it still runs if the computer was down at the timer of trigger
     };
   };
 
@@ -347,9 +369,9 @@ in
     };
     podman = {
       enable = true;
-      dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
     };
+    docker.enable = true;
     virtualbox.host = {
       enable = true;
     };
