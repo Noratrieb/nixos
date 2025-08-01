ab209064e4c826c27ab6cb32debbe73f7d99fde0
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-08-01 17:43:44 +0200

    prepare laptop

---
+++ nixos/paperless.nix
@@ -0,0 +1,23 @@
+{ lib, pkgs, ... }: {
+  systemd.services.paperless-ngx-backup = {
+    description = "paperless-ngx data backup to NAS";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "nora";
+      ExecStart = ''
+        ${lib.getExe pkgs.rsync} -a -v --delete --exclude=redis /home/nora/.local/share/paperless-ngx/ /mnt/nas/HEY/_Nora/paperless/backup
+      '';
+    };
+  };
+  systemd.timers.paperless-ngx-backup = {
+    description = "paperless-ngx data backup to NAS";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      Unit = "paperless-ngx-backup.service";
+      OnCalendar = "daily UTC";
+      RandomizedDelaySec = 1800;
+      FixedRandomDelay = true;
+      Persistent = true; # ensure it still runs if the computer was down at the timer of trigger
+    };
+  };
+}
